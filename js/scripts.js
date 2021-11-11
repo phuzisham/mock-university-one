4d57c475216c79ddc699769a71997c176904c90f
David Hale <david.hale7@gmail.com>
2021-11-11 15:17:42 -0800

    move files from gitlab

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// Carousel
+$('.carousel-item', '.show-neighbors').each(function(){
+  var next = $(this).next();
+  if (! next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+}).each(function(){
+  var prev = $(this).prev();
+  if (! prev.length) {
+    prev = $(this).siblings(':last');
+  }
+  prev.children(':nth-last-child(2)').clone().prependTo($(this));
+});
+// End Carousel
+
+// Search Bar
+$(".search-nav .btn").click(function(e){
+  e.preventDefault()
+  $(".search-nav").toggleClass("active");
+});
+// End Search bar
+
